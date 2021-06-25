3ebeb5fb67987f073e09d3f8b18c6b9aac9e92a6
mino89 <pierdomenicoreitano@gmail.com>
2021-06-25 17:20:19 +0200

    first commit

---
+++ video-browser/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+export default {
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList
+  },
+  methods: {
+    onTermChange(serchTerm) {
+        axios.get('https://www.googleapis.com/youtube/v3/search',{
+            params:{
+                key: process.env.VUE_APP_API_KEY,
+                type: 'video',
+                part: 'snippet',
+                q: serchTerm
+            }
+        }).then(response => {
+            console.log(response);
+        })
+
+        console.log(process.env.VUE_APP_API_KEY)
+    },
+  },
+};
+</script>
